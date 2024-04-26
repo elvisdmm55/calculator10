6393325e250ac3f67011bc1f49bd27d5ec25b059
administrador <elvisdmm55@educastur.es>
2024-04-26 16:28:52 +0200

    Añadido esqueleto Spring Boot

---
+++ src/main/java/com/ciber10/calculator10/Calculator10Application.java
@@ -0,0 +1,13 @@
+package com.ciber10.calculator10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciber10/calculator10/Calculator10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciber10.calculator10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
